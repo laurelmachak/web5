04ea9c0db57cad5c6efffcd270e04d497dcf4f8d
Laurel Machak <laurelmachak@gmail.com>
2018-02-28 14:10:52 -0800

    inline style, ugly still

--- api-project/src/products.js
+++ api-project/src/products.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import inventory from './inventory'
 
-
+const productStyles = {
+    width: "200px",
+    border: "1px solid black"
+}
 
 class Products extends Component {
     
@@ -11,7 +14,7 @@ class Products extends Component {
     
     render(){
         const items = inventory.map((item) =>{
-            return <div key={item.id}>{item.name} {item.price} {item.category}</div>
+            return <div key={item.id} style={productStyles}>{item.name} {item.price} {item.category}</div>
         });
         return (
             <div>{items}</div>
